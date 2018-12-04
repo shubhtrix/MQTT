b38ea97d0efb8e926abd9414cbc9846680815ed7
Shubham <shubham.sharma1@inventum.net>
2018-12-04 10:29:03 +0530

    Fixed the problem with publishing RESULT.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ libmosquitto is a C based library for MQTT protocol implementation and usage.
 This is a simple MQTT client over libmosquitto, with the facility to optionally daemonize 
 the client subscriber. 
 
-TO Do: 
+Done:
+1. Added support for publishing data after subscribing, publishing on RESULT. 
+
+To Do: 
 1. Need to add support for port and ip address under getopt.
-2. Requirement for publisher to publish any error logs back to the host system.
--- mqtt_client.c
+++ mqtt_client.c
@@ -1,10 +1,18 @@
 #include "mqtt_client.h"
 
 /* Callback for connection. */
-void on_connect (struct mosquitto *clnt_isnt, void *obj, int ret) {
+void on_connect (struct mosquitto *clnt_inst, void *obj, int ret) {
 
-	printf("\n");
-	//printf("Connect call-back result : %d\n", ret);
+	printf ("\n");
+	printf ("Connect call-back result : %d\n", ret);
+}
+
+/* Callback for publish. */
+void on_publish (struct mosquitto *clnt_inst, void *obj, int ret) {
+
+	printf ("\n");
+	printf ("Publish call-back result : %d\n", ret);
+
 }
 
 /* Callback for messages. */
@@ -12,37 +20,34 @@ void on_message (struct mosquitto *clnt_inst, void *obj, const struct mosquitto_
 
 	char *post_obj = NULL;
 	int *mid = NULL;
-	char *id = "NETCONF";
+	char *topic = "RESULT";
 	char *string = "OOPS!!!";
 
-	printf("Topic :: %s Message Length :: %d\nReceived Message :: %s\n", 
+	printf("Topic :: %s Message Length :: %d Received Message :: %s\n", 
 			 msg->topic, msg->payloadlen, (char*) msg->payload);
 
 	if (msg->payloadlen) {
 		
 		//post_obj = (char *)malloc(sizeof(char)*101);
 		post_obj = (char *)malloc(sizeof(int)*(msg->payloadlen));
-		//memset(post_obj,'\0',strlen(post_obj));
-		//printf("Final Data to post to clixon : %s\n", post_obj);	
-		//strcpy(post_obj,msg->payload);
-		//printf("Final Data to post to clixon : %s\n", post_obj);	
-		sprintf(post_obj,"curl -X PUT -d '%s' http://127.0.0.1/restconf/data", (char *)msg->payload);	
-		printf("Final Data to post to clixon : %s\n", post_obj);	
-		printf("Final Data Length : %ld\n", strlen(post_obj));	
-		syslog (LOG_INFO, "POSTED DATA :: %s", post_obj);
+		memset( post_obj, '\0', strlen( post_obj));
 		
-		printf("_____________________________________________________________________\n");	
+		sprintf( post_obj, "curl -X PUT -d '%s' http://127.0.0.1/restconf/data",
+			   	(char *)msg->payload);	
+		printf("Data to post to clixon :Length %ld\t%s\n", strlen(post_obj), post_obj);	
 
-		//mosquitto_publish ( clnt_inst, NULL, id, 21, "TEST STRING!!!", 0, true);
-		//mosquitto_publish ( clnt_inst, NULL, id, strlen(string), string, 0, true);
+		//syslog (LOG_INFO, "POSTED DATA :: %s", post_obj);
+		
+		memset( post_obj, '\0', strlen( post_obj));
+		sprintf( post_obj, "Data receieved successfully.");	
 
-		printf("_____________________________________________________________________\n");	
+		mosquitto_publish ( clnt_inst, NULL, topic, strlen(post_obj), post_obj, 0, false);
 
         free(post_obj);
 
 	} else {
-		printf("NO MESSAGE RECEIVED.\n");
-		//mosquitto_publish (clnt_inst, mid, "NETCONF", (int)strlen(post_obj), "TEST STRING!!!", 0, true);
+		//printf("NO MESSAGE RECEIVED.\n");
+		//mosquitto_publish (clnt_inst, mid, topic, (int)strlen(post_obj), "NO MESSAGE RECEIVED!!! :(", 0, false);
 	}
 }
 
@@ -92,7 +97,6 @@ void daemonize (void )
 			return;
 		}
 
-
 	}
 
 	PID = fork();
@@ -134,7 +138,7 @@ void daemonize (void )
 void default_client (void) 
 {
     int ret, qos=1;
-	char *id = "NETCONF";
+	char *id = "RESTCONF";
 
     // true to tell the boker to clean all messages on disconnect
 	// false to keep them under function mosquitto_new
@@ -158,20 +162,23 @@ void default_client (void)
 
 		mosquitto_connect_callback_set ( clnt_inst, on_connect);
 		mosquitto_message_callback_set ( clnt_inst, on_message);
+		mosquitto_publish_callback_set ( clnt_inst, on_publish);
 
-		ret = mosquitto_connect (clnt_inst, "localhost", 1883, 100);
+		//ret = mosquitto_connect ( clnt_inst, "localhost", 1883, 100);
+		ret = mosquitto_connect ( clnt_inst, "172.16.1.105", 1883, 100);
 		if (ret == MOSQ_ERR_SUCCESS) {
-			printf("Connect call SUCCESS.\n");	
-			printf("Connected waiting for Published Messages \n");
-			ret = mosquitto_subscribe(clnt_inst, NULL, id, qos);
+			printf ("Connect call SUCCESS.\n");
+			printf ("Connected waiting for Published Messages \n");
+			ret = mosquitto_subscribe( clnt_inst, NULL, id, qos);
 			if (ret == MOSQ_ERR_SUCCESS) {
 
-				ret = mosquitto_loop_forever (clnt_inst, -1, 1);
+				printf("mosquitto_connect SUCCESS.\n");	
+				ret = mosquitto_loop_forever ( clnt_inst, -1, 1);
 			}
-		} else if (ret == MOSQ_ERR_INVAL) {
+		} else if ( ret == MOSQ_ERR_INVAL) {
 			printf("Connect call invalid parameters.\n");	
 			return;
-		} else if (ret == MOSQ_ERR_ERRNO) {
+		} else if ( ret == MOSQ_ERR_ERRNO) {
 			printf("Connect call ERROR.\n");	
 			return;
 		}
@@ -180,6 +187,9 @@ void default_client (void)
 		mosquitto_destroy (clnt_inst);			
 
 	}
+	
+	/* Destroy the CLIENT Instance */
+	mosquitto_destroy (clnt_inst);			
 
 	/* Library Clean-up */
 	ret = mosquitto_lib_cleanup();
